5bac0fa40c2219af74f0f8e870266d4ca5616555
juandelgado4 <juan.delgado4@utp.edu.co>
2026-07-15 05:46:20 -0500

    fix(main): bypass TLS reject unauthorized for lyrics scraper (v1.2.4)

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const { app, BrowserWindow, ipcMain, shell } = require('electron');
 const { spawn } = require('child_process');
 const readline = require('readline');
